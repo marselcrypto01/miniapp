bf055b2a4febc61b1be6766893cda00e2f9d63a4
MarseL <marselvasiliev011@gmail.com>
2025-09-09 15:22:08 +0300

    pravki

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   eslint: {
     // не падать из-за ESLint на прод-сборке (Cloudflare Pages)
     ignoreDuringBuilds: true,
